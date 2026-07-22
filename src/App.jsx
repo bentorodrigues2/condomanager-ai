aea9ad57c411e388d4c9d1e9bd6f52f5b83203be
Tres Cab Wine <jcafguerra@gmail.com>
2026-07-22 23:19:16 +0100

    fix: remove video elements from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,6 @@ import Layout from "./ui/layout/Layout";
 
 export default function App() {
     return (
-    
-  <video
-    src="/assets/videos/logo-animation.mp4"
-    autoPlay
-    loop
-    muted
-    playsInline
-    className="logo-video"
-  />
-
-
-      
-<div className="video-wrapper">
-  <video
-    src="/assets/video//assets/video/logo-animation.mp4"
-    autoPlay
-    loop
-    muted
-    playsInline
-    className="logo-video"
-  />
-</div>
-
-    
         <AuthProvider>
             <Layout>
                 <AppRouter />
