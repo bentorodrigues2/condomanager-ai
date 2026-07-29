0d1350398862442765b2eb38ff5a7ebec2124799
Tres Cab Wine <jcafguerra@gmail.com>
2026-07-29 01:15:03 +0100

    Remove rotas CRUD e rebuild

--- src/router.jsx
+++ src/router.jsx
@@ -23,25 +23,26 @@ export default function AppRouter() {
                 <Route path="/auditor" element={<ProtectedRoute><AuditorDashboard /></ProtectedRoute>} />
                 <Route path="/contabilidade" element={<ProtectedRoute><ContabilidadeDashboard /></ProtectedRoute>} />
 
-                <Route path="/create/condominios" element={<CRUD.CreateCondominios />} />
-                             <Route path="/edit/condominios/:id" element={<CRUD.EditCondominios />} />
-<Route path="/create/fracoes" element={<CRUD.CreateFracoes />} />
-                             <Route path="/edit/fracoes/:id" element={<CRUD.EditFracoes />} />
-<Route path="/create/condominos" element={<CRUD.CreateCondominos />} />
-                             <Route path="/edit/condominos/:id" element={<CRUD.EditCondominos />} />
-<Route path="/create/fornecedores" element={<CRUD.CreateFornecedores />} />
-                             <Route path="/edit/fornecedores/:id" element={<CRUD.EditFornecedores />} />
-<Route path="/create/pagamentos" element={<CRUD.CreatePagamentos />} />
-                             <Route path="/edit/pagamentos/:id" element={<CRUD.EditPagamentos />} />
-<Route path="/create/incidencias" element={<CRUD.CreateIncidencias />} />
-                             <Route path="/edit/incidencias/:id" element={<CRUD.EditIncidencias />} />
-<Route path="/create/tarefas" element={<CRUD.CreateTarefas />} />
-                             <Route path="/edit/tarefas/:id" element={<CRUD.EditTarefas />} />
-<Route path="/create/assembleias" element={<CRUD.CreateAssembleias />} />
-                             <Route path="/edit/assembleias/:id" element={<CRUD.EditAssembleias />} />
-<Route path="/create/documentos" element={<CRUD.CreateDocumentos />} />
-                             <Route path="/edit/documentos/:id" element={<CRUD.EditDocumentos />} />
+                } />
+                             } />
+} />
+                             } />
+} />
+                             } />
+} />
+                             } />
+} />
+                             } />
+} />
+                             } />
+} />
+                             } />
+} />
+                             } />
+} />
+                             } />
             </Routes>
         </BrowserRouter>
     );
 }
+
