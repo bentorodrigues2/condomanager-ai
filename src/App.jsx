fa57c243f3556de1b655cd7df37712ae3ac6f4fd
Tres Cab Wine <jcafguerra@gmail.com>
2026-08-01 02:14:33 +0100

    Fix AppRouter import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./router/AppRouter";
+import AppRouter from "./router/AppRouter.jsx";
 
 export default function App() {
   return (
