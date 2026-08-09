f729a7d61e4009cde7f40b18a460a536cdbe4a1b
Tres Cab Wine <jcafguerra@gmail.com>
2026-08-09 16:58:50 +0100

    Fix App.jsx duplicate imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./router/AppRouter.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AppRouter />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
   );
 }
