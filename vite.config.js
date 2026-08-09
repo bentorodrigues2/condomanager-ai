7d5ea0e87aa46a7ddba891d86b57de701d5a2604
Tres Cab Wine <jcafguerra@gmail.com>
2026-08-09 12:25:38 +0100

    Fix manifest copy and build input

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".",
+  publicDir: "public",
   base: "",
   build: {
-    copyPublicDir: true,
     outDir: "dist",
-    emptyOutDir: true
+    emptyOutDir: true,
+    copyPublicDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html")
+      }
+    }
   }
 });
